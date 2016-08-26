c5955be48559f358d6a87da50900f2b92970f862
Hussard <adrien.ricartnoblet@gmail.com>
2016-08-26 16:02:02 +0200

    Update ExtraMimeTypeExtensionGuesser.php with findExtension() and comments

--- src/Symfony/ExtraMimeTypeExtensionGuesser.php
+++ src/Symfony/ExtraMimeTypeExtensionGuesser.php
@@ -2,18 +2,46 @@
 
 namespace MimeTyper\Symfony;
 
-if (!class_exists("Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesserInterface")) {
-    class_alias(
-        "Madhouse\HttpFoundation\File\MimeType\ExtensionGuesserInterface",
-        "Mimetyper\Symfony\ExtensionGuesserInterface"
-    );
-} else {
+if (class_exists("Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesserInterface")) {
     class_alias(
         "Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesserInterface",
         "Mimetyper\Symfony\ExtensionGuesserInterface"
     );
+} else {
+    /*
+     * Some project with PHP 5.3+ compatibility forced the fork of symfony/http-foundation.
+     * Look away, don't judge, bisous.
+     */
+    class_alias(
+        "Madhouse\HttpFoundation\File\MimeType\ExtensionGuesserInterface",
+        "Mimetyper\Symfony\ExtensionGuesserInterface"
+    );
 }
 
+/**
+ * Wrapper class for Symfony / Laravel UploadedFile extension guessing.
+ *
+ * ExtensionGuesser being a singleton, you can register this ExtensionGuesser
+ * like this:
+ *
+ *   use Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesser;
+ *
+ *   use MimeTyper\Repository\ExtensiveRepository;
+ *   use MimeTyper\Symfony\ExtraMimeTypeExtensionGuesser;
+ *
+ *   $symfonyGuesser = ExtensionGuesser::getInstance();
+ *   $extraGuesser = new ExtraMimeTypeExtensionGuesser(
+ *     new ExtensiveRepository()
+ *   );
+ *
+ *   $symfonyGuesser->register($extraGuesser);
+ *
+ * That way, you can enjoy the extended mapping, way more complete than default
+ * php array from symfony.
+ *
+ * @since 0.1.0
+ * @see Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesser
+ */
 class ExtraMimeTypeExtensionGuesser implements ExtensionGuesserInterface
 {
     /**
@@ -30,11 +58,6 @@ class ExtraMimeTypeExtensionGuesser implements ExtensionGuesserInterface
 
     public function guess($type)
     {
-        $matchingExtensions = $this->repository->findExtensions($type);
-        if ($matchingExtensions) {
-            // Return the first match.
-            return $matchingExtensions[0];
-        }
-        return null;
+        return $this->repository->findExtension($type);
     }
 }
